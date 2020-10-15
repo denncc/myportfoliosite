a589051e46d63c0e9c40421bea6ff2a1da56677a
denncc <56007045+dennnn16@users.noreply.github.com>
2020-10-14 22:41:58 -0700

    stage

---
+++ src/components/contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contacts</h1>
+            </div>
+        )
+    }
+}
