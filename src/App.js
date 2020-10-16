435896661bb7fed8883cd2bb2e368af6e8874db4
denncc <56007045+dennnn16@users.noreply.github.com>
2020-10-16 15:08:37 -0700

    daily commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className = "header-color" title="Title" scroll>
             <Navigation>
                 <Link to = "/">Home</Link>
                 <Link to = "/resume">Resume</Link>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style = {{width: '100%', margin: 'auto'}}>
+                <Grid className = "landing-grid">
+                    <Cell col={12}>
+                        <img src = "https://images.unsplash.com/photo-1540724487045-f220d0fe3861?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjIxNTV9"
+                        alt = "avatar"
+                        className = "avatar-img"
+                        />
+                        <div className = "banner-text">
+                            <h1>A Man Living His Life</h1>
+                            <hr/>
+                            <p>Specialty1 | Specialty2 | Specialty3 | Specialty4</p>
+                            <div className = "social-links">
+                                {/* LinkedIn */}
+                                <a href = "https://www.linkedin.com/in/dennis-chengchen-wu-6528171ab" target = "_blank" rel = "noopener noreferer" >
+                                    <i style={{color:"white", fontSize: "5em", padding: "8px"}} 
+                                    className = "fa fa-linkedin-square" aria-hidden = "true"/>
+                                </a>
+
+                                {/* GitHub */}
+                                <a href = "https://github.com/denncc" target = "_blank" rel = "noopener noreferer" >
+                                    <i style={{color:"white", fontSize: "5em", padding: "8px"}} 
+                                    className = "fa fa-github-square" aria-hidden = "true"/>
+                                </a>
+                                {/* Medium */}
+                                <a href = "https://medium.com/@denncc" target = "_blank" rel = "noopener noreferer" >
+                                    <i style={{color:"white", fontSize: "5em", padding: "8px"}} 
+                                    className = "fab fa-medium" aria-hidden = "true"/>
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
